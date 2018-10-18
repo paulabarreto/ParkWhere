3a617fd50eab08d65030761c2838728eac992139
donny1103 <donny1103@hotmail.com>
2018-10-18 15:40:54 -0700

    added the navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import NavBar from './components/Nav.jsx'
+class App extends Component {
+  render() {
+    return (
+      <NavBar />
+    );
+  }
+}
+
+export default App;
