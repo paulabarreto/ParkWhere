2892c4fae1a00214690b233e582cf69d9c10a36e
donny1103 <donny1103@hotmail.com>
2018-10-19 16:39:49 -0700

    test axios endpoint call

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import NavBar from './components/Nav.jsx'
 import Map from './components/Map.jsx'
-
+import axios from 'axios';
 class App extends Component {
+
+  componentDidMount() {
+    axios.get("https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='New_England_Patriots'")
+      .then(res => {
+        console.log(res)
+      })
+  }
+
   render() {
     return (
       <div>
