3b257052cb28e13f8de28b5b02d7d8318804397b
paulabarreto <paulavilaca@gmail.com>
2018-10-20 21:20:03 +0000

    fixed a bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import Nav from './Nav.jsx';
 import Map from './Map.jsx';
+import axios from 'axios';
+
 
 
 class Home extends Component {
+  state = {
+    coords: []
+  }
+  componentDidMount() {
+
+    axios.get("http://localhost:8080/",{
+      params: {
+        ID: 12345
+      }
+    })
+      .then(res => {
+        this.setState({coords:res.data})
+        //console.log(this.state)
+      })
+  }
+
   render() {
     return (
       <div>
         <Nav />
-        < Map />
+        < Map coords={this.state.coords}/>
       </div>
 
     );
