8414dab97d9fe14794896a072cf012e64c92c4ba
paulabarreto <paulavilaca@gmail.com>
2018-10-22 20:08:01 +0000

    local changes

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+class Login extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default Login;
