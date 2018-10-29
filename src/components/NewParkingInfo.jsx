38e22f0463a7d42325585544a2e6c86dadf97cec
donny1103 <donny1103@hotmail.com>
2018-10-28 20:48:31 -0700

    update new parking info tab with ant desgin

--- src/components/NewParkingInfo.jsx
+++ src/components/NewParkingInfo.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Well, FormGroup, InputGroup, FormControl }from 'react-bootstrap';
-import { Input, Select, TimePicker, Slider, Icon, Button} from 'antd';
+import { Select, TimePicker, Slider, Icon, Button} from 'antd';
 import moment from 'moment';
 import 'antd/dist/antd.css';
 
@@ -8,17 +8,37 @@ const Option = Select.Option;
 
 class NewParkingInfo  extends Component {
   state = {
-    date:'',
-    from:'',
-    to:'',
-    dateArr:[]
+    forms:{info1:{date:'', from:'', to:''}},
+    formNum:1
   }
-  handleRateChange = (rate) => {
-    this.setState({ rate });
+
+  addFormNum = () => {
+    let num = this.state.formNum + 1;
+    let info = {...this.state.forms}
+    info['info'+num] =  {date:'', from:'', to:''};
+    this.setState(prevstate=>({...prevstate, forms:info, formNum:num}));
+  }
+
+  removeFormNum = () => {
+    delete this.state.forms['info'+this.state.formNum];
+    let num = this.state.formNum - 1;
+    this.setState(prevstate=>({...prevstate, formNum:num}));
   }
 
   onRateChange = key => value => {
     this.props.onChange(key, value);
+  }
+
+  onDateSelect = index => value => {
+    let forms = {...this.state.forms};
+    forms['info'+index]['date'] = value;
+    this.setState(prevstate=>({...prevstate, forms:forms}));
+  }
+
+  onTimeSelect = (index,key) => value => {
+    let info = {...this.state.forms};
+    info['info'+index][key] = value? value.format('h:mm a') : '';
+    this.setState(prevstate=>({...prevstate, forms:info}));
   }
 
   render(){
@@ -33,17 +53,23 @@ class NewParkingInfo  extends Component {
       this.props.polyline.setDraggable(false);
       this.props.clearPoly();
     }
+
     const onSubmit = (e) => {
       e.preventDefault();
-      this.props.onSubmit();
+      //this.props.onSubmit();
       this.props.polyline.setEditable(false);
       this.props.polyline.setDraggable(false);
       this.props.onCondChange('isSubmitInfoOpen',false);
+
+      let datearr = []
+      for(let key in this.state.forms){
+        datearr.push(`${this.state.forms[key].date} ${this.state.forms[key].from} to ${this.state.forms[key].to}`)
+        console.log(datearr)
+      }
     }
     const onChange = key => e => {
       this.props.onChange(key, e.target.value);
     }
-    //const onClick = key => e => (this.props.onChange(key, e));
 
     const inputValue = (key) => (
       this.props.dynline[key] ? this.props.dynline[key] : ''
@@ -54,6 +80,45 @@ class NewParkingInfo  extends Component {
       dolllarDiv.push(<Icon type="dollar" style={{color:'#EEBA4C', fontSize:30}}/>)
     }
 
+    const formSelectDiv = (idx) => (
+      <div>
+        <Select 
+        defaultValue="Select date" 
+        style={{ width: 120}} 
+        onChange={this.onDateSelect(idx)}
+        >
+          <Option value="Mon-Fri">Mon-Fri</Option>
+          <Option value="Saturday">Saturday</Option>
+          <Option value="Sunday" >Sunday</Option>
+        </Select>
+        <TimePicker
+          className='start-time'
+          placeholder='from' 
+          style={{ width: 100}} 
+          use12Hours 
+          format="h:mm a" 
+          minuteStep={10}
+          defaultOpenValue={moment('08:00', 'h:mm a')}
+          onChange={this.onTimeSelect(idx,'from')}
+        />
+        {' '} ~ {' '}
+        <TimePicker
+          className='end-time' 
+          placeholder='to'
+          style={{ width: 100}} 
+          use12Hours 
+          format="h:mm a" 
+          minuteStep={10}
+          defaultOpenValue={moment('00:00', 'h:mm p')}
+          onChange={this.onTimeSelect(idx,'to')}
+        />        
+      </div>
+    )
+
+    const formSelectArr = [];
+    for(let i = 0; i < this.state.formNum; i++){
+      formSelectArr.push(formSelectDiv(i+1));
+    }
     return  (
       <Well className={this.props.classname}>
         <p>Nearby Address:</p>
@@ -70,37 +135,9 @@ class NewParkingInfo  extends Component {
                 />
             </InputGroup>
           </FormGroup>
-            <Select 
-            defaultValue="Select date" 
-            style={{ width: 120}} 
-            onChange={value=>(this.setState({date:value}))}
-            >
-              <Option value="Mon-Fri">Mon-Fri</Option>
-              <Option value="Saturday">Saturday</Option>
-              <Option value="Sunday" >Sunday</Option>
-            </Select>
-            <TimePicker
-              className='start-time'
-              placeholder='from' 
-              style={{ width: 100}} 
-              use12Hours 
-              format="h:mm a" 
-              minuteStep={10}
-              defaultOpenValue={moment('00:00', 'h:mm a')}
-              onChange={value=>(this.setState({from:value?value.format('h:mm a'):''}))}
-            />
-            {' '} ~ {' '}
-            <TimePicker
-              className='end-time' 
-              placeholder='to'
-              style={{ width: 100}} 
-              use12Hours 
-              format="h:mm a" 
-              minuteStep={10}
-              defaultOpenValue={moment('00:00', 'h:mm a')}
-              onChange={value=>(this.setState({to:value?value.format('h:mm a'):''}))}
-            />
-            <Button icon="plus"/>            
+            {formSelectArr.map(form=>(form))}
+            <Button icon="plus" onClick={this.addFormNum}/>
+            {this.state.formNum>1? <Button icon="minus" onClick={this.removeFormNum}/> : ''}         
             <br/><br/>
             <div className="slider-wrapper">
             {rate === 0 ? <Icon type="dollar" style={{color:'rgba(0, 0, 0, .45)', fontSize:30}}/> : dolllarDiv.map(element =>(<span>{element}</span>))}
@@ -113,7 +150,7 @@ class NewParkingInfo  extends Component {
             </div>
             <br/><br/>                 
           <Button onClick={onCancel}>Cancel</Button>
-          <Button type='submit'>Submit</Button>
+          <Button type='submit' onClick={onSubmit}>Submit</Button>
         </form>
         <br/>
       </Well>
